36cdaefb4277eedeb27784333d8b457622cc1856
nabilnir <nirarhan@gmail.com>
2025-11-13 22:17:22 +0600

    middle ware added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 run().catch(console.dir);
 
