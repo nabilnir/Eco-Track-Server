8c413fef8986112d39bbaa13352b2b1c9b33a1b3
nabilnir <nirarhan@gmail.com>
2025-11-13 22:19:27 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ecotrack0.zoz8wuc.mongodb.net/?appName=EcoTrack0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 run().catch(console.dir);
 
 app.listen(port, () => {
